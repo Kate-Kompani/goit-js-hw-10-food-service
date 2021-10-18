474b3bb325dccb5eccf076336dcc2cac444ef98c
Kateryna Kompaniiets <ketkacompani@gmail.com>
2021-10-18 22:26:09 +0300

    add menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,2 +1,13 @@
+import menuCard from '../templates/menu.hbs'
 import data from '../menu.json'
+
 console.log(data);
+console.log(menuCard);
+
+const menu = document.querySelector('.js-menu');
+let menuCardTpl = menuCard(data);
+
+menu.insertAdjacentHTML('beforeend', menuCardTpl);
+// function createCards(data) {
+//     return data.map(menuCard).join('');
+// }
